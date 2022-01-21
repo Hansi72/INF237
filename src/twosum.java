e6f22d97c36d9b2ae6e009825f3ebe22f9939108
Hans Trygve <trygvehans@gmail.com>
2022-01-21 14:43:51 +0100

    no message

--- src/twosum.java
+++ src/twosum.java
@@ -8,67 +8,70 @@ import java.io.PrintWriter;
 import java.io.OutputStream;
 
 public class twosum {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Kattio io = new Kattio(System.in, System.out);
-        io.println(io.getInt()+io.getInt());
+        io.println(io.getInt() + io.getInt());
         io.close();
     }
-}
 
-class Kattio extends PrintWriter {
-    public Kattio(InputStream i) {
-        super(new BufferedOutputStream(System.out));
-        r = new BufferedReader(new InputStreamReader(i));
-    }
-    public Kattio(InputStream i, OutputStream o) {
-        super(new BufferedOutputStream(o));
-        r = new BufferedReader(new InputStreamReader(i));
-    }
+    static class Kattio extends PrintWriter {
+        public Kattio(InputStream i) {
+            super(new BufferedOutputStream(System.out));
+            r = new BufferedReader(new InputStreamReader(i));
+        }
 
-    public boolean hasMoreTokens() {
-        return peekToken() != null;
-    }
+        public Kattio(InputStream i, OutputStream o) {
+            super(new BufferedOutputStream(o));
+            r = new BufferedReader(new InputStreamReader(i));
+        }
 
-    public int getInt() {
-        return Integer.parseInt(nextToken());
-    }
+        public boolean hasMoreTokens() {
+            return peekToken() != null;
+        }
 
-    public double getDouble() {
-        return Double.parseDouble(nextToken());
-    }
+        public int getInt() {
+            return Integer.parseInt(nextToken());
+        }
 
-    public long getLong() {
-        return Long.parseLong(nextToken());
-    }
+        public double getDouble() {
+            return Double.parseDouble(nextToken());
+        }
 
-    public String getWord() {
-        return nextToken();
-    }
+        public long getLong() {
+            return Long.parseLong(nextToken());
+        }
+
+        public String getWord() {
+            return nextToken();
+        }
 
 
+        private BufferedReader r;
+        private String line;
+        private StringTokenizer st;
+        private String token;
 
-    private BufferedReader r;
-    private String line;
-    private StringTokenizer st;
-    private String token;
-
-    private String peekToken() {
-        if (token == null)
-            try {
-                while (st == null || !st.hasMoreTokens()) {
-                    line = r.readLine();
-                    if (line == null) return null;
-                    st = new StringTokenizer(line);
+        private String peekToken() {
+            if (token == null)
+                try {
+                    while (st == null || !st.hasMoreTokens()) {
+                        line = r.readLine();
+                        if (line == null) return null;
+                        st = new StringTokenizer(line);
+                    }
+                    token = st.nextToken();
+                } catch (IOException e) {
                 }
-                token = st.nextToken();
-            } catch (IOException e) { }
-        return token;
-    }
+            return token;
+        }
 
-    private String nextToken() {
-        String ans = peekToken();
-        token = null;
-        return ans;
+        private String nextToken() {
+            String ans = peekToken();
+            token = null;
+            return ans;
+        }
     }
 }
+
+
 
