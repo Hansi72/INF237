ff080010c6b0776c5cc741f203d07cded6c26e84
Hans Trygve <trygvehans@gmail.com>
2022-02-22 12:39:41 +0100

    Spiderman with linkedlist return

--- src/Spiderman.java
+++ src/Spiderman.java
@@ -1,144 +1,148 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Spiderman {
     static Spiderman.Kattio io = new Spiderman.Kattio(System.in, System.out);
-    static int[] climbs;
-    static LinkedList<Integer>[] memoized;
-    static LinkedList<Queue<Integer>> pathsToZero = new LinkedList();
-    static LinkedList<Queue<Integer>> shortcuts = new LinkedList();
-    static LinkedList<Integer>[] shortcutInfo;
+    static HashMap<String, LinkedList<Integer>> memoized = new HashMap();
 
-    //fixme finn alle nullpaths, sett sammen paths med memoized, såå regn ut høyden per path og velg laveste.
     public static void main(String[] args) {
+
+/*
         int tasks = io.getInt();
-        for (int task = 0; task < tasks; task++) {
-            System.out.println("task " + task);
-            climbs = new int[io.getInt()];
-            memoized = new LinkedList[climbs.length];
-            shortcutInfo = new LinkedList[climbs.length];
-            for (int climb = 0; climb < climbs.length; climb++) {
-                memoized[climb] = new LinkedList();
-                shortcutInfo[climb] = new LinkedList();
-                climbs[climb] = io.getInt();
+        for(int i = 0; i < tasks; i++){
+            int climbs = io.getInt();
+            Stack<Integer> heights = new Stack();
+            for(int k = 0; k < climbs; k++){
+                heights.push(io.getInt());
             }
-            Queue<Integer> path = new LinkedList();
-            path.add(climbs[0]);
-            climb(1, climbs[0], new LinkedList());
+            printAnswer(climb(heights, heights.peek()));
+
         }
+*/
+        LinkedList<Integer> st1 = new LinkedList();
+        st1.push(20);
+        st1.push(20);
+        st1.push(20);
+        st1.push(20);
 
-        Queue<Integer> currentPath;
-        System.out.println("shortcuts");
-        LinkedList<String> bestShortcuts;
-        while(!shortcuts.isEmpty()){
-            //todo sammenlign shortcuts og finn de med lavest topphøyde (for lignende shortcuts. bruk SET?)
-            currentPath = shortcuts.pop();
-            //System.out.println(shortcutInfo[currentPath.size()].pop());
-            System.out.println(currentPath);
-        }
-        /*
-        pathtoZero er nå eks: 3-1-6-3-2-0
-        shortcut eks: 3-5-0-3
-        shortcut eks2: 3-6-0-3
-        shortcut eks3: 3-2-3-1-4
-        todo
-        for hver pathtozero
-                for pathtozero[i]
-                    if shortcutinfo[i].contains == pathtozero[i];   //todo bytte til en mer effektiv runtime for .contains()?
-                            for tilsvarende shortcuts, finn den med lavest maxtall (husk å først sammenlign med pathtozero)
+        //LinkedList<Integer> result =
+                climb(st1);
 
+        //printAnswer(result);
 
-
-
-        */
-
-        System.out.println("paths to zero");
-        while(!pathsToZero.isEmpty()){
-            System.out.println(pathsToZero.pop());
-            //todo gå gjennom pathen og hvis shortcuts matcher høyden og i, sammenlign topphøyden på disse.
-        }
+        System.out.println("memosize "+ memoized.size());
+        System.out.println(memoized);
 
         io.close();
 
     }
-    //todo ---------------------
-    static public void climb(int i, int height, Queue<Integer> path) {
-        if (i == climbs.length) {
-            System.out.println("end. height = " + height + " path: " + path);
-            if (height == 0) {
-                pathsToZero.push(path);
-                return;
-            } else {
-                //todo do something?
-                return;
+
+    static void printAnswer(LinkedList<Integer> result) {
+        if (result == null) {
+            io.println("IMPOSSIBLE");
+        } else {
+            //io.print("U");
+            while(result.size() > 2){
+                if (result.pop() > result.peek()) {
+                    io.print("D");
+                } else {
+                    io.print("U");
+                }
             }
+            //io.print("D");
+            io.println("");
         }
-
-        if (memoized[i].contains(height)) {
-            //todo lagre topheight og path sammen? hvis paths som er lagret har lik start, ta den med lavest topheight
-            shortcuts.push(path);
-            shortcutInfo[i].push(height);
-            return;
-        }
-        memoized[i].push(height);//todo
-
-       // System.out.println("im at: " + i + " with height: " + height + "  and topheight: " + topHeight[i]);
-
-        //todo bytt ut string concatenation med en stack el lignende.
-        if (height - climbs[i] >= 0) {
-                path.add(height - climbs[i]);
-                climb(i + 1, height - climbs[i], path);
-                path.remove();
-        }
-        path.add(height + climbs[i]);
-        climb(i + 1, height + climbs[i], path);
-
 
     }
 
-
-
-    static public void climbOld(int i, int height, String path) {
-        if (i == climbs.length) {
-            System.out.println("end. height = " + height + " path: " + path);
-            if (height == 0) {
-                //pathsToZero.push(path);
-                return;
+    static LinkedList<Integer> climb(LinkedList<Integer> heights) {
+        if (heights.size() == 1) {
+            if (heights.get(0) == 0) {
+                LinkedList<Integer> returnList = new LinkedList();
+                returnList.push(0);
+                returnList.push(0);
+                return returnList;
             } else {
-                //todo do something?
-                return;
+                return null;
             }
         }
-
-        if (memoized[i].contains(height)) {
-            //todo lagre topheight og path sammen? hvis paths som er lagret har lik start, ta den med lavest topheight
-            //shortcuts.push(path);
-            shortcutInfo[i].push(height);
-            return;
+        if (heights.peek() < 0) {
+            return null;
         }
-        memoized[i].push(height);//todo
+        String heightsKey = heights.toString();
+        if(memoized.containsKey(heightsKey)) {
 
-        // System.out.println("im at: " + i + " with height: " + height + "  and topheight: " + topHeight[i]);
+            if (memoized.get(heightsKey) != null) {
+                System.out.println("---------------start-----------------");
+                System.out.println("Currently at::::  Heights");
+                for (int i = 0; i < heights.size(); i++) {
+                    System.out.print(heights.get(i) + ", ");
+                }
+                System.out.println("");
 
-        //todo bytt ut string concatenation med en stack el lignende.
-        if (height - climbs[i] >= 0) {
-            int res = height - climbs[i];
-            climbOld(i + 1, height - climbs[i], path + "D");
+                System.out.println("and returning MEMO, results");
+                for (int i = 0; i < memoized.get(heightsKey).size(); i++) {
+                    System.out.print(memoized.get(heightsKey).get(i) + ", ");
+                }
+                System.out.println("");
+                System.out.println("---------------end-----------------");
+                return memoized.get(heights);
+            }
+            return null;
         }
-        climbOld(i + 1, height + climbs[i], path + "U");
 
+
+
+        //create return array
+        LinkedList<Integer> output;
+
+        LinkedList<Integer> upStack = new LinkedList((LinkedList) heights.clone());
+        LinkedList<Integer> downStack = new LinkedList((LinkedList) heights.clone());
+
+        upStack.push(upStack.pop() + upStack.pop());
+        downStack.push(downStack.pop() - downStack.pop());
+
+        output = leastHeighted(climb(upStack), climb(downStack));
+
+        if (output == null) {
+            memoized.put(heightsKey, null);
+            return null;
+        }
+
+        int currentHeight = heights.peek();
+        output.push(currentHeight);
+
+        if(output.peekLast() < currentHeight){
+            output.pollLast();
+            output.offerLast(currentHeight);
+        }
+
+        System.out.println("heigths :" + heights.toString());
+        System.out.println("output :" + output.toString());
+
+        memoized.put(heightsKey, new LinkedList<>());
+        for(int i = 0; i < output.size(); i++){
+            //while(!output.isEmpty()){
+            memoized.get(heightsKey).offerLast(output.get(i));
+        }
+        System.out.println("memoized: "+ memoized );
+        System.out.println("-----------------------");
+        return memoized.get(heightsKey);
     }
 
+    static LinkedList<Integer> leastHeighted(LinkedList<Integer> stack1, LinkedList<Integer> stack2) {
+        if (stack1 == null) {
+            return stack2;
+        }
+        if (stack2 == null) {
+            return stack1;
+        }
 
-
-
-
-    class shortcut{
-        String path;
-        int maxHeight;
+        if (stack1.peek() < stack2.peek()) {
+            return stack1;
+        } else {
+            return stack2;
+        }
     }
 
     static class Kattio extends PrintWriter {
