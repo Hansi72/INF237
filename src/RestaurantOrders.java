5a91a33fb04d576866cff809899acc6741dd8af7
Hans Trygve <trygvehans@gmail.com>
2022-02-23 16:42:51 +0100

    RestaurantOrders complete (pre-set and linkedList iteration)

--- src/RestaurantOrders.java
+++ src/RestaurantOrders.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 
 public class RestaurantOrders {
     static RestaurantOrders.Kattio io = new RestaurantOrders.Kattio(System.in, System.out);
+    static int targetIndex;
 
     public static void main(String[] args) {
 
@@ -21,91 +22,74 @@ public class RestaurantOrders {
             LinkedList<int[]>[] order = new LinkedList[io.getInt() + 1];
             order[0] = new LinkedList();
 
-            //fixme remove me
-            if(taskIndex > 0){
-                continue;
-            }//fixme remove me
-
-
-            for (int orderIndex = 0; orderIndex < order.length; orderIndex++) {
+            //fill first targetIndexes
+            for (int menuIndex = 0; menuIndex < menu.length; menuIndex++) {
+                targetIndex = menu[menuIndex];
+                order[targetIndex] = new LinkedList<int[]>();
+                order[targetIndex].push(new int[1]);
+                order[targetIndex].peek()[0] = menu[menuIndex];
+            }
+            for (int orderIndex = 1; orderIndex < order.length; orderIndex++) {
+                //end early if ambiguous
+                if(order[order.length-1] != null){
+                if(order[order.length-1].size() > 2){
+                    break;
+                }}
                 if (order[orderIndex] != null) {
-
                     //menuloop
                     for (int menuIndex = 0; menuIndex < menu.length; menuIndex++) {
-
                         //if target is not out of bounds
-                        int targetIndex = menu[menuIndex] + orderIndex;
+                        targetIndex = menu[menuIndex] + orderIndex;
                         if (targetIndex < order.length) {
-                            //if target is null, create linkedList and add menu[menuIndex] to it and push it to orders;
                             if (order[targetIndex] == null) {
                                 order[targetIndex] = new LinkedList<int[]>();
-                                //order[targetIndex].push(new int[1]);
-                               // order[targetIndex].peek()[0]= menu[menuIndex];
                             }
-                                //order
-                            if(order[orderIndex].isEmpty()){
-                                order[targetIndex].push(new int[1]);
-                                order[targetIndex].peek()[0]= menu[menuIndex];
-                            }else {
-                                for (int[] orderList : order[orderIndex]) {
-                                    order[targetIndex].push(new int[orderList.length + 1]);
-                                    for (int i = 0; i < orderList.length; i++) {
-                                        order[targetIndex].peek()[i] = orderList[i];
-                                    }
-                                    order[targetIndex].peek()[order[targetIndex].peek().length - 1] = menu[menuIndex];
+                            //push orderIndex arrays to targetIndex
+                            for (int[] orderList : order[orderIndex]) {
+                                order[targetIndex].push(new int[orderList.length + 1]);
+                                for (int i = 0; i < orderList.length; i++) {
+                                    order[targetIndex].peek()[i] = orderList[i];
                                 }
+                                order[targetIndex].peek()[order[targetIndex].peek().length - 1] = menu[menuIndex];
                             }
-                            //order[targetIndex].push(new int[1]);
-                            // order[targetIndex].peek()[0]= menu[menuIndex];
-
-
-
-
-
-
-
                         }
                     }
-
-                    //fixme remove me
-                    System.out.println("-----------  orderIndex::"+orderIndex);
-                    for(int i = 0; i < order.length; i++) {
-                        System.out.print("current order[" + i + "] = [");
-                        if(order[i] != null){
-                            for(int k = 0; k < order[i].size(); k++) {
-                                System.out.print("[");
-                                for(int num:order[i].get(k)){
-                                    System.out.print(num + ", ");
-                                }
-                                System.out.print("]");
-                            }
-                        }
-                        System.out.println("]");
-                    }
-                    //fixme remove me
-
-
-
                 }
+                //fixme remove me
+                System.out.println("-----------  orderIndex::" + orderIndex);
+                for (int i = 0; i < order.length; i++) {
+                    System.out.print("current order[" + i + "] = [");
+                    if (order[i] != null) {
+                        for (int k = 0; k < order[i].size(); k++) {
+                            System.out.print("[");
+                            for (int num : order[i].get(k)) {
+                                System.out.print(num + ", ");
+                            }
+                            System.out.print("]");
+                        }
+                    }
+                    System.out.println("]");
+                }
+                //fixme remove me
             }
-            if (order[order.length - 1] != null) {
-                System.out.print("current order[" + (order.length-1) + "] = [");
-                if(order[order.length-1] != null){
-                    for(int k = 0; k < order[order.length-1].size(); k++) {
-                        System.out.print("[");
-                        for(int num:order[order.length-1].get(k)){
-                            System.out.print(num + ", ");
-                        }
-                        System.out.print("]");
-                    }
+
+
+            if(order[order.length-1] != null){
+                System.out.println("task: " +taskIndex+" finished size = " + order[order.length-1].size());
+            if(order[order.length-1].size() < 4){
+                for(int i =0; i < order[order.length-1].peekLast().length-1;i++){
+                    io.print(order[order.length-1].peekLast()[i] + " ");
                 }
-                System.out.println("]");
+                //io.println(order[order.length-1].peekLast()[order[order.length-1].peekLast().length-1]);
+            }else{
+                io.println("Ambiguous");
+            }
+            }else{
+                io.println("Impossible");
             }
 
 
         }
-
-
         io.close();
     }
 
