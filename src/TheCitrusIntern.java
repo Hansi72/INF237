eef1a1f8f8165ffef4628eb0424a039ff5af7d65
Hans Trygve <trygvehans@gmail.com>
2022-04-14 21:44:27 +0200

    TheCitrusIntern complete

--- src/TheCitrusIntern.java
+++ src/TheCitrusIntern.java
@@ -4,52 +4,79 @@ import java.util.*;
 public class TheCitrusIntern {
     static TheCitrusIntern.Kattio io = new TheCitrusIntern.Kattio(System.in, System.out);
     static long[] pick;
-    static long[] skip;
+    static long[] skipBelow;  //cost of skipping current node if dominated from below
+    static long[] skipAbove;  //cost of skipping current node if dominated from above
     static int[] costs;
     static int[][] subs;
+    static long inf = Long.parseLong("8223372036854775807");
 
     public static void main(String[] args) {
         //setup
         int memberCount = io.getInt();
         costs = new int[memberCount];
         subs = new int[memberCount][];
+        boolean[] isSub = new boolean[memberCount];
+        int sub;
         for (int i = 0; i < memberCount; i++) {
-            costs[i] = io.getInt();;
+            costs[i] = io.getInt();
             subs[i] = new int[io.getInt()];
             for (int j = 0; j < subs[i].length; j++) {
-                subs[i][j] = io.getInt();
+                sub = io.getInt();
+                subs[i][j] = sub;
+                isSub[sub] = true;
+            }
+        }
+        //find sour excellence (root)
+        int sourExcellence = -1;
+        for (int i = 0; i < memberCount; i++) {
+            if (!isSub[i]) {
+                sourExcellence = i;
             }
         }
         //work
         pick = new long[memberCount];
-        skip = new long[memberCount];
+        skipAbove = new long[memberCount];
+        skipBelow = new long[memberCount];
 
-        work(5);
-
-        io.println(pick[5]);
-        io.println(skip[5]);
-
-
+        work(sourExcellence);
+        io.println(Math.min(skipBelow[sourExcellence], pick[sourExcellence]));
         io.close();
-
-
-
     }
 
-    static void work(int member){
-        System.out.println("currently at " + member);
+    static void work(int member) {
+        pick[member] = costs[member];
+        skipAbove[member] = 0;
+
         //if leaf node
-        if(subs[member].length == 0){
-            pick[member] = costs[member];
-            skip[member] = 0;
-        }else{
-            long sum = 0;
-            for(int sub : subs[member]){
+        if (subs[member].length == 0) {
+            skipBelow[member] = inf;
+        } else {
+            long sumSubDominatedFromAbove = 0;
+            long sumSubDominatedFromBelow = 0;
+            for (int sub : subs[member]) {
                 work(sub);
-                sum = sum + skip[sub];
+                sumSubDominatedFromAbove = sumSubDominatedFromAbove + skipAbove[sub];
+                sumSubDominatedFromBelow = sumSubDominatedFromBelow + Math.min(pick[sub], skipBelow[sub]);
             }
-            pick[member] = sum + costs[member];
-            skip[member] = Math.max(pick[member], sum);
+            pick[member] = pick[member] + sumSubDominatedFromAbove;
+            skipAbove[member] = sumSubDominatedFromBelow;
+
+            //pick the lowest cost of getting dominated from below
+            long cost = 0;
+            long promote = inf;
+            boolean hasDominatorBelow = false;
+            for (int sub : subs[member]) {
+                cost = cost + Math.min(pick[sub], skipBelow[sub]);
+                promote = Math.min(promote, Math.max(0, pick[sub] - skipBelow[sub]));
+                if (pick[sub] < skipBelow[sub]) {
+                    hasDominatorBelow = true;
+                }
+            }
+            if (hasDominatorBelow) {
+                skipBelow[member] = cost;
+            } else {
+                skipBelow[member] = cost + promote;
+            }
         }
     }
 
