4536798dd805555eb8ff55b7f0ec8eed89763c1a
Hans Trygve <trygvehans@gmail.com>
2022-04-22 16:22:42 +0200

    Paintball complete (slow. Using boolean version of a maxflow algorithm. bipartite matching would be faster.)

--- src/Paintball.java
+++ src/Paintball.java
@@ -3,38 +3,69 @@ import java.util.*;
 
 public class Paintball {
     static Paintball.Kattio io = new Paintball.Kattio(System.in, System.out);
-    static boolean[] visited;
+    static boolean[][] G;
 
     public static void main(String[] args) {
 
         int playerCount = io.getInt();
         int M = io.getInt();
-        if (M < 1) { //todo
-            io.println("Impossible");
-        }
-        boolean[][] G = new boolean[playerCount+2][playerCount+2];
-        int[][] RG = new int[playerCount+2][playerCount+2];
-        visited = new boolean[playerCount+2];
+        G = new boolean[playerCount * 2 + 2][playerCount * 2 + 2];
         int A;
         int B;
         for (int i = 0; i < M; i++) {
             A = io.getInt();
             B = io.getInt();
-            G[A][B] = true;
-            G[B][A] = true;
+            G[A][B + playerCount] = true;
+            G[B][A + playerCount] = true;
         }
-        //add flow and paths to source and sink (last two indices respectively)
-        for(int i = 0; i < playerCount-2; i++){
-            RG[playerCount][i] = 1;
-            G[playerCount][i] = true;
-            G[i][playerCount+1] = true;
+        //add paths to source and sink (first and last indices respectively)
+        for (int i = 1; i < playerCount+1; i++) {
+            G[0][i] = true;
+            G[playerCount + i][G.length - 1] = true;
         }
 
+        //while there is a path to target(sink) in the residual network.
+        while (flow(0, new boolean[playerCount * 2 + 2])) {
+        }
 
+        //find answer from Graph and output.
+        LinkedList<Integer> answer = new LinkedList<>();
+        for (int j = playerCount; j < G.length - 1; j++) {
+            for (int i = 1; i < playerCount + 1; i++) {
+                if (G[j][i]) {
+                    answer.push(i);
+                }
+            }
+        }
 
-
-
+        if (answer.size() < playerCount) {
+            io.println("Impossible");
+        } else {
+            Iterator answerIter = answer.descendingIterator();
+            while (answerIter.hasNext()) {
+                io.println(answerIter.next());
+            }
+        }
         io.close();
+    }
+
+    static boolean flow(int node, boolean[] visited) {
+        visited[node] = true;
+        //return true if we hit target (sink)
+        if (node == G.length - 1) {
+            return true;
+        }
+        for (int i = 0; i < G.length; i++) {
+            if (G[node][i] && !visited[i]) {
+                //if we are returning from target(sink), remove current edge and put backwards edges while returning to Source.
+                if (flow(i, visited)) {
+                    G[node][i] = false;
+                    G[i][node] = true;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     static class Kattio extends PrintWriter {
