83a805f6214a2e7a44dc7c913da7212b773506f2
Hans Trygve <trygvehans@gmail.com>
2022-05-20 15:59:46 +0200

    SemiPrimeHNumbers complete

--- src/SemiPrimeHNumbers.java
+++ src/SemiPrimeHNumbers.java
@@ -5,34 +5,54 @@ public class SemiPrimeHNumbers {
     static SemiPrimeHNumbers.Kattio io = new SemiPrimeHNumbers.Kattio(System.in, System.out);
 
     public static void main(String[] args) {
-        int input = io.getInt();
         boolean[] sieve;
-        while(input != 0){
-            sieve = new boolean[(input-1)/4];
-            for(int i = 1; i < sieve.length; i++){
-                if(!sieve[i]){
-                    int multiple = 2;
-                    while(multiple*(4*i+1) < input){
-                        System.out.println("setting non prime " + multiple*(4*i+1) + " index " + (multiple*(4*i+1)-1)/4);
-                        sieve[(multiple*(4*i+1)-1)/4] = true;
-                        multiple++;
+        int[] input = new int[100000];
+        int inputMax = 0;
+        int currentInput = io.getInt();
+        int index = 0;
+        //gather all inputs to find max input.
+        while (currentInput != 0) {
+            input[index] = currentInput;
+            inputMax = Math.max(inputMax, currentInput);
+            currentInput = io.getInt();
+            index++;
+        }
+        inputMax++;
+
+        //sieve of eratosthenes on '4n+1 numbers'
+        sieve = new boolean[inputMax];
+        for (int i = 5; i < Math.sqrt(inputMax); i = i + 4) {
+            if (!sieve[i]) {
+                int multiple = 2;
+                while (multiple * i < inputMax) {
+                    sieve[multiple * i] = true;
+                    multiple++;
+                }
+            }
+        }
+
+        //find all semiPrimes (5 is the first H-prime)
+        int[] semiPrimePrefixSum = new int[sieve.length];
+        for (int i = 5; i < inputMax / 5; i = i + 4) {
+            if (!sieve[i]) {
+                for (int j = i; j < inputMax / i + 1; j = j + 4) {
+                    if (!sieve[j]) {
+                        semiPrimePrefixSum[j * i] = 1;
                     }
                 }
             }
-            //sieve of eratosthenes complete
-
-
-
-
-
-
-
-            input = io.getInt();
+        }
+        //scan to complete prefix sum values.
+        for (int i = 5; i < inputMax; i = i + 4) {
+            semiPrimePrefixSum[i] = semiPrimePrefixSum[i] + semiPrimePrefixSum[i - 4];
         }
 
-
-
-
+        //output
+        int i = 0;
+        while (input[i] != 0) {
+            io.println(input[i] + " " + semiPrimePrefixSum[input[i]]);
+            i++;
+        }
         io.close();
     }
 
